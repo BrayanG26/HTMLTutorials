5f8a452b86f01532f62a62f087db6653105bd289
BrayanG26 <brayanguerrero4040@gmail.com>
2018-02-23 16:56:17 -0500

    Detail of selected row

--- Tables/main.js
+++ Tables/main.js
@@ -1,2 +1,16 @@
 var tabla = document.getElementsByClassName("tabla-frutas");
+var caja = document.getElementById("visible");
+console.log(caja);
 console.log(tabla);
+var celdas = document.getElementsByClassName("price");
+// console.log(celdas);
+for (var i = 0; i < celdas.length; i++) {
+    celdas[i].addEventListener("click", function (event) {
+        // console.log(event);
+        precio = document.getElementById("precioFruta");
+        nombre = document.getElementById("nombreFruta");
+        nombre.innerHTML = event.target.previousElementSibling.innerHTML;
+        precio.innerHTML = event.target.textContent;
+        caja.style.display = "inline-block";
+    }, false);
+}
